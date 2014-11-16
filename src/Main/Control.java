212c11ae567cd50a5de36f9e9ff7c66144504c39
Tobias Jacobsen <tobias.cbs@gmail.com>
2014-11-16 11:21:52 +0100

    working from home: fixing getRandomQuestion-method

--- src/Main/Control.java
+++ src/Main/Control.java
@@ -12,8 +12,8 @@ public class Control implements WordPairControlInterface {
     //declare 5 word lists. 1 is high priority and down to 5 which is low priority 
     LinkedList<WordPair> wordList;
 
-    String chosenWordpairText;
-    String wordpairText;
+    String chosenWordpair;
+    String text;
 
     public Control() {
         wordList = new LinkedList<WordPair>();
@@ -38,50 +38,63 @@ public class Control implements WordPairControlInterface {
         Random roll = new Random();
         int randomPriority;
         do {
-            do {
-                randomPriority = roll.nextInt(15) + 1;
-                switch (randomPriority) {
-                    case 1:
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 5: //1-5 high priority
-                        randomPriority = 1;
-                        break;
-                    case 6:
-                    case 7:
-                    case 8:
-                    case 9:
-                        randomPriority = 2;
-                        break;
-                    case 10:
-                    case 11:
-                    case 12:
-                        randomPriority = 3;
-                        break;
-                    case 13:
-                    case 14:
-                        randomPriority = 4;
-                        break;
-                    case 15:
-                        randomPriority = 5; //low priority
-                        break;
+//----------------------------------------------------------------            
+//find a random number between 1-15 and assign to 'randomPriority'
+//----------------------------------------------------------------
+            randomPriority = roll.nextInt(15) + 1; 
+            switch (randomPriority) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5: //1-5 high priority
+                    randomPriority = 1;
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    randomPriority = 2;
+                    break;
+                case 10:
+                case 11:
+                case 12:
+                    randomPriority = 3;
+                    break;
+                case 13:
+                case 14:
+                    randomPriority = 4;
+                    break;
+                case 15:
+                    randomPriority = 5; //low priority
+                    break;
+            }
+            System.out.println("1. priority is: " + randomPriority);
+//---------------------------------------------------------------------
+//run through wordList and find all wordpairs equal to 'randomPriority'
+//put those in 'randomList' and set foundIt to true
+//---------------------------------------------------------------------
+            for (int i = 0; i < wordList.size(); i++) {
+                int index = wordList.get(i).getPriority();
+                if (index == randomPriority) {
+                    randomList.add(wordList.get(i));
+                    foundIt = true;
                 }
-                System.out.println("1. priority is: " + randomPriority);
-                for (int i = 0; i < wordList.size(); i++) {
-                    int index = wordList.get(i).getPriority();
-                    if (index == randomPriority) {
-                        randomList.add(wordList.get(i));
-                        foundIt = true;
-                    }
-                }
-            } while (foundIt == false);
-            int arrayIndex = roll.nextInt(randomList.size());
-            WordPair wordpair = randomList.get(arrayIndex);
-            wordpairText = wordpair.toString();
-        } while (chosenWordpairText == wordpairText); //no!, this is fucked!
-        chosenWordpairText = wordpairText;
-        return wordpairText;
+            }
+        } while (foundIt == false);
+//---------------------------------------------------------------------------------
+//assign a random index-position to arrayIndex, based on the size of the randomList
+//take a wordpair from this index and assign to a wordpair object
+//assign that particular question to 'chosenWordpair'
+//assign string wordpair to text
+//----------------------------------------------------------------------------------
+        int arrayIndex = roll.nextInt(randomList.size());
+        WordPair wordpair = randomList.get(arrayIndex);
+        chosenWordpair = wordpair.getQuestion();
+        //while (wordpair.getQuestion ==    /// <----------- work on this!
+        
+        text = wordpair.getWordpair();
+        return text;
     }
 
     @Override
